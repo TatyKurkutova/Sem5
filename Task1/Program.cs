9ad9f032215577dcb50a7fd36b8bc2cfbf3fe573
Tatiana <tatianka.dmitrieva.96@mail.ru>
2023-09-11 23:37:17 +0300

    Количество четных чисел в массиве из трехзначных чисел

---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] numbers = new int[4];
+int count = 0;
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    count++;
+}
+
+
+Console.WriteLine($"количество чётных чисел в массиве равно {count} ");
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-999,1000);
+    }
+}
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
